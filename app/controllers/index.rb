9d1a2ab2974e723fd1929aeb592c175a3a3de6a6
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-05 09:29:19 -0500

    .

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -9,4 +9,6 @@ post '/color' do
 
   cell= rand(1..9)
   color= "#" + "%06x" % (rand * 0xffffff)
+  content_type :json
+  { cell: cell, color: color }.to_json
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
-
+  $('#get_color').click(function(event) {
+    event.preventDefault();
+    var url = ('/color');
+    $.post(url, function(data, status, server){
+      console.log(data);
+      $('#color_me li:nth-child(' + data['cell'] + ')').css('background', data['color']);
+    }, 'json');
+  });
 });
